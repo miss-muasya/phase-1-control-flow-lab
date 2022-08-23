7363b19e1f83178c358d0d4c6bdfc12bb9acf9a5
miss-muasya <sheemuasya@gmail.com>
2022-08-23 14:42:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance<=400){
+    return 'This one is on me!'
+  }
+  else if (distance>2000 && distance<2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance>2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+console.log(scuberGreetingForFeet(400));
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+ if (city === 'NYC') {
+    return "Ok, sounds good." 
+  }
+  else if (city != 'NYC') {
+    return "No go."   
+  }
+
 }
 
-function switchOnCharmFromTip(){
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  if (tip === 'generous') {
+    return "Thank you so much."
+  }
+  else if (tip === 'not as generous') {
+    return "Thank you."
+  }
+  else{
+    return "Bye."
+  }
 }
